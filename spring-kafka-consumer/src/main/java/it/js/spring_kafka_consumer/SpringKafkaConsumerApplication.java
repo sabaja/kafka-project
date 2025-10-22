b3d278a321abac52f5b9f1e21d5b80d6da533ba7
U0N9369 <jacopo.sabatini@esternibisp.com>
2025-10-22 16:50:07 +0200

    Fix kafka consumer project

--- spring-kafka-consumer/src/main/java/it/js/spring_kafka_consumer/SpringKafkaConsumerApplication.java
+++ spring-kafka-consumer/src/main/java/it/js/spring_kafka_consumer/SpringKafkaConsumerApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringKafkaConsumerApplication {
 
-    static void main(String[] args) {
-        SpringApplication.run(SpringKafkaConsumerApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(SpringKafkaConsumerApplication.class, args);
+	}
 
 }
